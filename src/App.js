2479997bdf3ebe7dd57ada31174487a58ada9562
Christopher Dougherty <christophertdoc@gmail>
2020-03-21 21:19:08 -0400

    lesson-3: partial

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react' 
 import Navbar from './components/Navbar'
 import BookList from './components/BookList'
+import ThemeToggle from './components/ThemeToggle'
 import { ThemeContext } from './contexts/ThemeContext'
 import { AuthContext } from './contexts/AuthContext'
-import ThemeToggle from './components/ThemeToggle'
-import AuthContextProvider from './contexts/AuthContext'
+
 
 class App extends Component {
 	render() {
 		return (
 			<AuthContext.Consumer>{(authContext) => (
 				<ThemeContext.Consumer>{(themeContext) => {
+					const { authenticated, toggleAuth } = authContext 
 					return (
 						<div className={`App ${themeContext.theme}`}>	
-							{ authContext.authenticated.toString() }	
+							<div onClick={ toggleAuth }>
+								{ authenticated ? 'Logged In' : 'Logged Out' }	
+							</div>
 							<Navbar />
 							<BookList />	
 							<ThemeToggle />		
