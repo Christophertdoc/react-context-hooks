ec7c357dea0c723fde293b890fbd50d83268a678
Christopher Dougherty <christophertdoc@gmail>
2020-03-30 00:47:11 -0400

    update uuid

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ From the following tutorial: https://www.youtube.com/watch?v=6RhOzQciVwI&list=PL
 - lesson-3: how to use two different contexts in the same component
 - lesson-4: how to use the useState() hook
 - lesson-5: how to use the useEffect() hook
+
+## Terms
+- Context: used for global state
+- hook: used to do a variety of different things in functional components
+- useState(): a hook that is used for state
+- useEffect(): Very similar to the "lifecycle methods" that are used in class components. useEffect() allows for the manipulation of functional components during "mounting", "updating" and "unmounting".
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
-import uuid  from 'uuid/v1'
+import React, { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import NewSongForm from './NewSongForm'
 
 const SongList = () => { 
+
     const [songs, setSongs] = useState([
         { title: 'Almost Home', id: 1 },
         { title: 'Memory Gospel', id: 2 },
         { title: 'This Wild Darkness', id: 3 }
     ])
+
     const addSong = (title) => {
-        setSongs([...songs, { title: title, id: uuid() }])
+        setSongs([...songs, { title: title, id: uuidv4() }])
     }
+
+    useEffect(() => {
+        console.log('useEffect hook ran', songs)
+    })
+
     return (
         <div className='songList'>
             <ul>
