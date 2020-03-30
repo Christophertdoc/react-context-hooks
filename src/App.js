05f43fbae32751effbd4e6ef10780651dfb468ce
Christopher Dougherty <christophertdoc@gmail>
2020-03-30 18:22:24 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import SongList from './components/SongList'
+import React, { Component } from 'react' 
+import Navbar from './components/Navbar'
+import BookList from './components/BookList'
+import ThemeToggle from './components/ThemeToggle'
+import { ThemeContext } from './contexts/ThemeContext'
+import { AuthContext } from './contexts/AuthContext'
 
-function App() {
-	return (
-		<div className="App">
-			<SongList />
-		</div>
-	)
+
+class App extends Component {
+	render() {
+		return (
+			<AuthContext.Consumer>{(authContext) => (
+				<ThemeContext.Consumer>{(themeContext) => {
+					const { authenticated, toggleAuth } = authContext 
+					return (
+						<div className={`App ${themeContext.theme}`}>	
+							<div onClick={ toggleAuth }>
+								{ authenticated ? 'Logged In' : 'Logged Out' }	
+							</div>
+							<Navbar />
+							<BookList />	
+							<ThemeToggle />		
+						</div>	
+					)	
+				}}</ThemeContext.Consumer>
+			)}</AuthContext.Consumer>
+		)
+	}
 }
 
 export default App
